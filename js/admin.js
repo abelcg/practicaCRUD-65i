8c0fce1d0358153fe5b83f355c52b71e7a2f6a99
abelcg <abelcg86@gmail.com>
2023-09-13 15:22:41 -0300

    add validations

---
+++ js/admin.js
@@ -0,0 +1,84 @@
+//traigo los elementos que necesito del html
+let campoCodigo = document.getElementById('codigo');
+console.log(campoCodigo);
+let campoProducto = document.getElementById('producto');
+let campoDescripcion = document.getElementById('descripcion');
+let campoCantidad = document.getElementById('cantidad');
+let campoURL = document.getElementById('URL');
+
+let formProductos = document.querySelector('#formProductos');
+
+//validaciones
+
+const campoRequerido = (input) => {
+  console.log('desde campo requerido');
+  console.log(input.value);
+  if (input.value.trim().length > 0) {
+    console.log('aqui esta todo bien');
+    input.className = 'form-control is-valid';
+    return true;
+  } else {
+    console.log('aqui muestro el error');
+    input.className = 'form-control is-invalid';
+    return false;
+  }
+};
+
+const validarNumeros = (input) => {
+  //vamos a usar o a crear una expresion regular
+  let patron = /^[0-9]{1,5}$/;
+  //el método test permite comparar un string con el patrón y devuelve true o false
+  //regex.test('string a validar')
+  if (patron.test(input.value)) {
+    //cumple con la expresoón regular
+    input.className = 'form-control is-valid';
+    return true;
+  } else {
+    input.className = 'form-control is-invalid';
+    return false;
+  }
+};
+
+const validarURL = (input) => {
+  let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+  if (patron.test(input.value)) {
+    input.className = 'form-control is-valid';
+    return true;
+  } else {
+    input.className = 'form-control is-invalid';
+    return false;
+  }
+};
+
+const validarGeneral = ()=>{
+
+}
+
+//Asociar un evento a cada elemento obtenido
+
+campoCodigo.addEventListener('blur', () => {
+  console.log('desde codigo');
+  campoRequerido(campoCodigo);
+});
+
+campoProducto.addEventListener('blur', () => {
+  console.log('desde producto');
+  campoRequerido(campoProducto);
+});
+
+campoDescripcion.addEventListener('blur', () => {
+  console.log('desde descripcion');
+  campoRequerido(campoDescripcion);
+});
+
+campoCantidad.addEventListener('blur', () => {
+  console.log('desde cantidad');
+  validarNumeros(campoCantidad);
+});
+
+campoURL.addEventListener('blur', () => {
+  console.log('desde url');
+  validarURL(campoURL);
+});
+
+formProductos.addEventListener('submit', )
