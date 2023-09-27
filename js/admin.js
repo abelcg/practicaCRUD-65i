229804ef33b8aad305515699f74c86a3bcfde70c
abelcg <abelcg86@gmail.com>
2023-09-27 09:47:50 -0300

    add index logic

--- js/admin.js
+++ js/admin.js
@@ -16,6 +16,7 @@ let campoCantidad = document.getElementById('cantidad');
 let campoURL = document.getElementById('URL');
 
 let formProductos = document.querySelector('#formProductos');
+let btnNuevo = document.getElementById('btnNuevo');
 
 let productoExistente = false; //variable bandera: si productoExistente es false quiero crear,
 //si es true quiero modificar el producto existente
@@ -52,6 +53,7 @@ campoURL.addEventListener('blur', () => {
 });
 
 formProductos.addEventListener('submit', guardarProducto);
+btnNuevo.addEventListener('click', limpiarFormulario)
 
 //Llamo a la función cargaInicial(): si tengo productos en el localStorage, los muestre en la tabla
 cargaInicial();
